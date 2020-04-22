3b67d875c4b9388d18ea883c820bd2637458d51a
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-04-21 23:35:54 -0300

    .gitignore changes

--- functions/index.js
+++ functions/index.js
@@ -9,15 +9,7 @@ admin.initializeApp({
   databaseURL: "https://socialape-6ffab.firebaseio.com"
 });
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA-XB4ih9K1wzx2XITcB44_3ZHNI_9cRCY",
-  authDomain: "socialape-6ffab.firebaseapp.com",
-  databaseURL: "https://socialape-6ffab.firebaseio.com",
-  projectId: "socialape-6ffab",
-  storageBucket: "socialape-6ffab.appspot.com",
-  messagingSenderId: "684740821979",
-  appId: "1:684740821979:web:4a769a7b053fe0fa68ddfe"
-};
+const firebaseConfig = require('./firebaseConfig.json');
 
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
