0386f6107b08c12df69fcb12dd2d397977660168
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-04-21 22:12:40 -0300

    sign up validation

--- functions/index.js
+++ functions/index.js
@@ -63,6 +63,17 @@ app.post('/scream', (req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if (string.trim() === '') return true;
+  else return false;
+}
+
+const isEmail = (email) => {
+  const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+}
+
 // Signup route
 app.post('/signup', (req, res) => {
   const newUser = {
@@ -71,6 +82,21 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   }
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty.';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address.';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty.';
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match.';
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty.';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json(errors);
+
 
   let token, userId;
   db.doc(`/users/${newUser.handle}`).get()
